238c4a46a8797e1ae1caaeae3499a323bcc20e30
emokid <karta7756@gmail.com>
2026-02-23 13:34:29 +0300

    unique, count values

--- main.py
+++ main.py
@@ -9,9 +9,14 @@ from keras.datasets import mnist
 print(f'Data train shape: {data_train.shape}\n'
       f'Target train shape: {target_train.shape}\n'
       f'Dtype: {data_train.dtype}\n'
-      f'Value range: {data_train.min()} - {data_train.max()}')
+      f'Value range: {data_train.min()} - {data_train.max()}\n')
 
 print(f'Data test shape: {data_test.shape}\n'
       f'Target test shape: {target_test.shape}\n'
       f'Dtype: {target_train.dtype}\n'
       f'Value range: {target_train.min()} - {target_train.max()}\n')
+
+# подсчет уникальных классов (целевых данных)
+unique, count = np.unique(target_train, return_counts = True)
+for digit, count in zip(unique, count):
+    print(f'Digit: {digit}, count: {count} - {count / len(target_train) * 100:.1f}%')
