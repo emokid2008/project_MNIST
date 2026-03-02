48ee24abeb73b2ad919d5434c79ea0d8f2da738f
emokid2008 <karta7756@gmail.com>
2026-03-02 13:12:52 +0300

    one-hot encoding & finish message

--- main.py
+++ main.py
@@ -63,3 +63,18 @@ print('After reshape:\n'
       f'single digit: {data_train_flat[0].shape}\n')
 
 print(f'Check: {28} * {28} = {28 * 28}')
+
+# One-hot encoding 
+target_train_cat = to_categorical(target_train, 10)
+target_test_cat = to_categorical(target_test, 10)
+
+print(f'\nBefore one-hot: {target_train.shape}\n'
+      f'After one-hot: {target_train_cat.shape}\n'
+      f'Example target[0]: {target_train[0]} -> {target_train_cat[0]}\n')
+
+# Завершение процесса
+print(f'Finished preprocessing\n'
+      f'data_train_flat: {data_train_flat.shape}\n'
+      f'data_test_flat: {data_test_flat.shape}\n'
+      f'target_train_cat: {target_train_cat.shape}\n'
+      f'target_test_cat: {target_test_cat.shape}\n')
